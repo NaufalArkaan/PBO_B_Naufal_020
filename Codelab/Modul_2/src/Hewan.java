7a763bba11c67db3f604872ba2beeaab852ffb60
NaufalArkaan <naufalarkaan25@gmail.com>
2025-03-05 23:02:21 +0700

    codelab 1 modul 2

---
+++ Codelab/Modul_2/src/Hewan.java
@@ -0,0 +1,20 @@
+package src;
+
+public class Hewan {
+    String Nama;
+    String Jenis;
+    String Suara;
+
+    Hewan(String Nama, String Jenis, String Suara){ //konstruktor
+        this.Nama = Nama;
+        this.Jenis = Jenis;
+        this.Suara = Suara;
+    }
+
+    public void tampilkanInfo(){
+        System.out.println("Nama: " + Nama);
+        System.out.println("Jenis: " + Jenis);
+        System.out.println("Suara: " + Suara);
+        System.out.print("\n");
+    }
+}
